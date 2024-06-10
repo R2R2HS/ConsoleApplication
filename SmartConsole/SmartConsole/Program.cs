76658e91c41342502a61e412ba161f7e0f8df6df
R2R2HS <84908244+R2R2HS@users.noreply.github.com>
2024-06-10 19:18:16 +0300

    Created Smart Console
    Великое начало!

---
+++ SmartConsole/SmartConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmartConsole
+{
+	internal class Program
+	{
+		const string error = "И нахуя ты это сюда вставил, придурок?";
+		const string hello = "Hello World!!!";
+
+		private static void Main(string[] args)
+		{
+			if (args.Length > 0)
+			{
+				Console.WriteLine(error);
+				return;
+			}
+
+			Console.WriteLine(hello);
+
+			//Это чтоб не закрывалось, пон?
+			Console.ReadLine();
+		}
+	}
+}
---
+++ SmartConsole/SmartConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("SmartConsole")]
+[assembly: AssemblyDescription("The smartest console application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Empty")]
+[assembly: AssemblyProduct("SmartConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("Empty")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(true)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0b33a4d3-62b1-492d-8d17-453f958b72c2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("ru-RU")]
